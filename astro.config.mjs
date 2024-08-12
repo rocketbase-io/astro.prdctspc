2d75370b4f2da774014e049f893983177d2196db
Marten <marten@rocketbase.io>
2024-08-12 12:26:48 +0200

    feat: added + modified seo + og-image generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,33 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
+import sitemap from '@astrojs/sitemap';
 import alpinejs from "@astrojs/alpinejs";
+
+import opengraphImages, {presets} from "astro-opengraph-images";
+
+import * as fs from "node:fs";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.productspace.io',
   integrations: [tailwind({
     applyBaseStyles: false
-  }), mdx(), alpinejs()],
+  }), mdx(), alpinejs(), sitemap(),
+    opengraphImages({
+      options: {
+        fonts: [
+          {
+            name: "Latinotype Mohr",
+            weight: 400,
+            style: "normal",
+            data: fs.readFileSync("public/fonts/latinotype_-_mohr_regular-webfont.woff"),
+          },
+        ],
+      },
+      render: presets.blackAndWhite,
+    }),
+  ],
   i18n: {
     defaultLocale: "de",
     locales: ["de", "en"]
